208feb3dff0c49865abaac4a887d88f17a9e5e2b
Maksim Zinchuk <maxim.zinchuk@gmail.com>
2020-06-16 11:31:13 +0300

    Исправления

--- script/script.js
+++ script/script.js
@@ -1,33 +1,32 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup'); 
 
 let UserName = document.querySelector('.profile__name');
 let UserLabel = document.querySelector('.profile__label');
+let UserNameEdit = document.querySelector('.popup__profile_name');
+let UserLabelEdit = document.querySelector('.popup__profile_label');
 
 let formElement = document.querySelector('.popup__container');
 const popupSaveButton = formElement.querySelector('.popup__save-button');
 
 //открытие попапа
 function popupOpen() {
-    const popup = document.querySelector('.popup'); 
     popup.classList.toggle('popup__opened');
-    document.querySelector('.popup__profile-name').value = UserName.textContent;
-    document.querySelector('.popup__profile-label').value = UserLabel.textContent;
+    UserNameEdit.value = UserName.textContent;
+    UserLabelEdit.value = UserLabel.textContent;
 }
 profileEditButton.addEventListener('click', popupOpen);
 
 //закрытие попапа
-function popupClose() {
-    const popupClose = document.querySelector('.popup'); 
-    popupClose.classList.remove('popup__opened');
+function popupClose() { 
+    popup.classList.remove('popup__opened');
 }
 popupCloseButton.addEventListener('click', popupClose);
 
 //отправка формы
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let UserNameEdit = document.querySelector('.popup__profile-name');
-    let UserLabelEdit = document.querySelector('.popup__profile-label');
     UserName.textContent = UserNameEdit.value;
     UserLabel.textContent = UserLabelEdit.value;
     popupClose();
